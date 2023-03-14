37433218fc52a3d7f52881e433342ccb76b304c7
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-03-14 19:22:41 +0800

    refactor: test the cookie in the parent

--- src/page/index/Home.tsx
+++ src/page/index/Home.tsx
@@ -35,6 +35,7 @@ const Home: React.FC = (props) => {
     return (<div></div>);
   }
 
+  const accessTokenCookie = document.cookie.split('; ').find(row => row.startsWith('accessToken='));
 
     return(<div>
       <GenieHeader onMenuClick = { (value) =>{
