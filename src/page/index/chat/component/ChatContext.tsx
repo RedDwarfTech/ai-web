83273c60b52449abce59228657fce25742dead15
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-03-19 15:56:08 +0800

    feat: add code block render

--- src/page/index/chat/component/ChatContext.tsx
+++ src/page/index/chat/component/ChatContext.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./ChatContext.css"
 import ReactMarkdown from 'react-markdown';
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { dark, oneDark, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { CodeProps } from "react-markdown/lib/ast-to-react";
 
 export interface IChatMsg {
     msg: string
@@ -10,9 +13,27 @@ const ChatContext: React.FC<IChatMsg> = (props) => {
     const message = props.msg;
     const msgRender = () => {
         if (message) {
-            return(<ReactMarkdown>
-                {message}
-            </ReactMarkdown>);
+            return (<ReactMarkdown children={message}
+                components={{
+                    code({ node, inline, className, children}) {
+                        const match = /language-(\w+)/.exec(className || '')
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                style={oneDark}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            >
+                                {String(children).replace(/\n$/, '')}
+                            </SyntaxHighlighter>
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        )
+                    }
+                }}
+            />);
         }
         return (<div></div>);
     }
