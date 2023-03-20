543d6811008e65171a57e040178e4a3ead61a4be
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-03-21 02:02:03 +0800

    refactor: tweak

--- src/config/app/config-pro.ts
+++ src/config/app/config-pro.ts
@@ -2,7 +2,7 @@ let proConfigMap = new Map([
   ["wssUrl", "wss://ai.poemhub.top/post/websocket"],
   ["logoutUrl", "https://ai.poemhub.top"],
   ["baseAuthUrl","https://ai.poemhub.top"],
-  ["accessTokenUrlPath","/ai/auth//access-token/refresh"]
+  ["accessTokenUrlPath","/ai/auth/access-token/refresh"]
 ]); 
 
 export default proConfigMap;
