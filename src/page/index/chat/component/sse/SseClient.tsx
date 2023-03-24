9c93822375412ad6781262aeb6892fd9f6b60647
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-03-24 23:27:01 +0800

    refactor: remove long text send

--- src/page/index/chat/component/sse/SseClient.tsx
+++ src/page/index/chat/component/sse/SseClient.tsx
@@ -4,22 +4,17 @@ import { v4 as uuid } from 'uuid';
 
 const SseClient: React.FC = (props) => {
     const [data, setData] = useState('');
-    const longText = "这是一段长文本...";
+    const longText = "long...";
 
     useEffect(() => {
         const accessToken = localStorage.getItem("x-access-token");
+        // https://stackoverflow.com/questions/6623232/eventsource-and-basic-http-authentication
         const eventSource = new EventSourcePolyfill('/ai/stream/chat/ask?q=hello', {
             headers: {
                 'x-access-token': accessToken ?? "",
                 'x-request-id': uuid(),
             }
         });
-        let index = 0;
-        while (index < longText.length) {
-            const chunk = longText.substring(index, Math.min(index + 1024, longText.length));
-            eventSource.dispatchEvent(new MessageEvent('message', { data: chunk }));
-            index += chunk.length;
-        }
         eventSource.onmessage = e => {
             alert(e.data);
             setData(e.data);
