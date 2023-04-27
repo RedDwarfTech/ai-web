8ae22e69f9fa28417e5e20af7829a1f5a25bfe1f
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-04-27 21:52:31 +0800

    refactor: tweak the product id

--- src/page/index/goods/Goods.tsx
+++ src/page/index/goods/Goods.tsx
@@ -27,7 +27,7 @@ const Goods: React.FC = (props: any) => {
 
   const handlePay = (row: any) => {
     let param = {
-      productId: row.id
+      productId: Number(row.id)
     };
     doPay(param);
   };
