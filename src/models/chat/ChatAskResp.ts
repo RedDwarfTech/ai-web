4c2db210fe8419d548b01ac728e53284549a7cce
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-16 23:04:45 +0800

    fix: fix the compile issue

--- src/models/chat/ChatAskResp.ts
+++ src/models/chat/ChatAskResp.ts
@@ -1,14 +1,4 @@
-import { IConversation } from "./3.5/Conversation"
-
 export interface IChatAskResp {
-    chatProps: {
-        chatResp: string
-    },
-    conversations: {
-        conversations: {
-            list: IConversation[]
-        }
-    },
     onMenuClick: (menu: String) => void;
     menu: String
 }
