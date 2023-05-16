e0f3425868827600d5c32476af1dee38f83e9fe1
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-17 00:51:57 +0800

    refactor: add page request parameter

--- src/service/chat/ConversationService.ts
+++ src/service/chat/ConversationService.ts
@@ -2,9 +2,11 @@ import { IConversationReq } from '@/models/request/conversation/ConversationReq'
 import { requestWithActionType } from '../../common/XHRClient';
 
 export function getConversations(params: IConversationReq) {
+    var queryString = Object.keys(params).map(key => key + '=' + params[key as keyof IConversationReq]).join('&');
+
     const config = {
         method: 'get',
-        url: '/ai/conversation/page',
+        url: "/ai/conversation/page?" + queryString,
         headers: {'Content-Type': 'application/json'},
         data: JSON.stringify(params)
     };
