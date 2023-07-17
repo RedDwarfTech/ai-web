fa714241aed22b6e488c9374c2070b12f8293b26
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-17 16:50:48 +0800

    refactor: tweak vendor

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,6 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes("react")){
-              return "react-vendor";
-            }
             return 'vendor';
           }
         }
