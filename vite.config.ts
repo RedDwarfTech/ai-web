cbb60a3db91c720dca4f0ada3147d2c26064b973
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-18 14:47:39 +0800

    refactor: try to add the compatible plugin to fixed the firefox compatible issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'node:path';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({})
+      ],
+    }
+  },
   build: {
     outDir: "build",
     rollupOptions: {
