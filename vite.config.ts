e28ee835dd57dac4a3d27afc5a81d98caf43b1fa
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-29 19:16:01 +0800

    feat: seperate public js with vendor

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import autoprefixer from 'autoprefixer';
+import { visualizer } from "rollup-plugin-visualizer";
+import { PluginOption } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: "test.html",
+      open:true 
+    }) as PluginOption
+  ],
   css: {
     postcss: {
       plugins: [
@@ -19,7 +30,8 @@ export default defineConfig({
       output: {
         manualChunks: {
           react: ['react','react-router-dom','react-dom'],
-          reddwarf: ['rd-component','rdjs-wheel']
+          reddwarf: ['rd-component','rdjs-wheel'],
+          vendor: ['axios','dayjs','hashmap','idb']
         }
       }
     }
