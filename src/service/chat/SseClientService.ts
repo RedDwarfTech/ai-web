c4a69792b21cb8ba46b5fa37d653c3e31ef68886
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-06-09 15:37:33 +0800

    feat: add chat msg listener

--- src/service/chat/SseClientService.ts
+++ src/service/chat/SseClientService.ts
@@ -1,7 +1,5 @@
 import { ChatAsk } from '@/models/request/chat/ChatAsk';
-import { EventSourcePolyfill } from 'event-source-polyfill';
 import { AuthHandler, RequestHandler } from 'rdjs-wheel';
-import { v4 as uuid } from 'uuid';
 
 export function doAskPreCheck(params: ChatAsk, onSseMessage: (msg: string, eventSource: EventSource) => void) {
   if (AuthHandler.isTokenNeedRefresh(60)) {
@@ -14,12 +12,15 @@ export function doAskPreCheck(params: ChatAsk, onSseMessage: (msg: string, event
   }
 }
 
+/**
+ * https://stackoverflow.com/questions/77015804/why-the-event-source-polyfill-did-not-fetch-the-sse-api-data
+ * @param params 
+ * @param onSseMessage 
+ */
 export function doSseChatAsk(params: ChatAsk, onSseMessage: (msg: string, eventSource: EventSource) => void) {
   let eventSource: EventSource;
-  const accessToken = localStorage.getItem("x-access-token");
   var queryString = Object.keys(params).map(key => key + '=' + params[key as keyof ChatAsk]).join('&');
   // https://stackoverflow.com/questions/6623232/eventsource-and-basic-http-authentication
-  var queryString = Object.keys(params).map(key => key + '=' + params[key as keyof ChatAsk]).join('&');
   eventSource = new EventSource('/ai/azure/stream/chat/ask?' + queryString);
   eventSource.onopen = () => {
 
@@ -31,4 +32,7 @@ export function doSseChatAsk(params: ChatAsk, onSseMessage: (msg: string, eventS
   eventSource.onmessage = e => {
     onSseMessage(e.data, eventSource);
   };
+  eventSource.addEventListener("chat", function (event: any) {
+    onSseMessage(event.data, eventSource);
+  });
 }
