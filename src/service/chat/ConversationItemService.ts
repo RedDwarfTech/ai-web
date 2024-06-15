0d68abd8734b5e0d412709f4babbd885e87b0519
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-06-15 12:47:41 +0800

    refactor: tweak the path of fetch conversation

--- src/service/chat/ConversationItemService.ts
+++ src/service/chat/ConversationItemService.ts
@@ -7,7 +7,7 @@ export function getConversationItems(params: IConversationItemReq) {
     var queryString = Object.keys(params).map(key => key + '=' + params[key as keyof IConversationItemReq]).join('&');
     const config = {
         method: 'get',
-        url: '/ai/conversation/item/page?' + queryString,
+        url: '/ai/conversation-item/page?' + queryString,
         headers: { 'Content-Type': 'application/json' }
     };
     const actionTypeString: string = ConversationItemActionType[ConversationItemActionType.CONVERSATION_ITEM_PAGE];
